93515651d564080172c90726174554d7fcb00e88
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-10-30 19:41:26 -0300

    trabalhando com state

--- react-test/src/components/PostList.jsx
+++ react-test/src/components/PostList.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
 import Post from "./Post"
 import classes from './';
 import NewPost from "./NewPost";
 
 function PostList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
             <ul className={classes.posts}>
-                <Post author="Daniel" body="React.js é maravilhoso" />
+                <Post author={enteredAuthor} body="React.js é maravilhoso" />
                 <Post author="Feranda" body="o curso me ajudou muito" />
             </ul>
         </>
