5d9a67a4ce8553c3aebe61b80f2ac16d75f94e55
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-10-31 22:51:03 -0300

    modal visivel

--- react-test/src/components/PostList.jsx
+++ react-test/src/components/PostList.jsx
@@ -6,8 +6,13 @@ import NewPost from "./NewPost";
 import Modal from './Modal';
 
 function PostList() {
+    const [modalVisible, setModalVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalVisible(falsee);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -18,9 +23,11 @@ function PostList() {
     }
     return (
         <>
-            <Modal>
-                <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
-            </Modal>
+            {modalVisible ? (
+                <Modal onClose={hideModalHandler()}>
+                    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+                </Modal>
+            ) : null}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body="React.js é maravilhoso" />
                 <Post author="Feranda" body="o curso me ajudou muito" />
