69263f110e6467b5aa400ba016620085f0ceeda1
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-11-06 23:48:51 -0300

    adicionando submit no componente new post

--- react-test/src/components/PostList.jsx
+++ react-test/src/components/PostList.jsx
@@ -6,25 +6,14 @@ import NewPost from "./NewPost";
 import Modal from './Modal';
 
 function PostList({ isPosting, onStopPosting }) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
     return (
         <>
             {isPosting ? (
                 <Modal onClose={onStopPosting}>
-                    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} onCancel={onStopPosting} />
+                    <NewPost onCancel={onStopPosting} />
                 </Modal>
             ) : null}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body="React.js é maravilhoso" />
                 <Post author="Feranda" body="o curso me ajudou muito" />
             </ul>
         </>
